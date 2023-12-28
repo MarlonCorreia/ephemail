194561db884505344020a1b21e7a8aab50fc5697
marloncorreia <malilocsimas26@gmail.com>
2023-12-28 13:58:19 -0300

    chore: Fix format

--- internal/clipb/clipb.go
+++ internal/clipb/clipb.go
@@ -2,13 +2,12 @@ package clipb
 
 import "golang.design/x/clipboard"
 
-
 func SendToClipBoard(msg string) error {
-    err := clipboard.Init()
-    if err != nil {
-        return err
-    }
+	err := clipboard.Init()
+	if err != nil {
+		return err
+	}
 
-    clipboard.Write(clipboard.FmtText, []byte(msg))
-    return nil
+	clipboard.Write(clipboard.FmtText, []byte(msg))
+	return nil
 }
--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/MarlonCorreia/ephemail/cmd"
 
 func main() {
-    cmd.InitCmd()
+	cmd.InitCmd()
 }
